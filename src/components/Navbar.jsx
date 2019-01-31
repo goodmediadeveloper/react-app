e72894238512aed43dac4760d68220d290f1cfe8
Dzmitry Prokharau <goodmediadeveloper@gmail.com>
2019-01-31 09:55:24 +0300

    31-01-2019 morning

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+// import "./App.css";
+
+const Navbar = () => {
+  return (
+    <nav className="nav">
+      <div>
+        <a>Messages</a>
+      </div>
+      <div>
+        <a>News</a>
+      </div>
+      <div>
+        <a>Musik</a>
+      </div>
+      <div>
+        <a>Settings</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
